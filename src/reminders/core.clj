0841818ac67d044733397545adc7f91c64e3f8c5
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-02-17 22:49:19 +0100

    Basic functionality

--- src/reminders/core.clj
+++ src/reminders/core.clj
@@ -1,1 +1,14 @@
-(ns reminders.core)
+(ns reminders.core
+  (:import [org.joda.time DateTime]))
+
+(defn add [desc when reminders]
+  (conj reminders [(new DateTime when) desc]))
+
+(defn filter-near [days reminders]
+  (letfn [(near? [date days]
+            (if (. (new DateTime) isAfter (. date minusDays days))
+              true
+              false))]
+    (sort (filter #(near? (first %) days) reminders))))
+
+;; TODO Use atoms to make this functions easier to use through a REPL
