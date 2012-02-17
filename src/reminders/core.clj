d880ed576791144b63abb683cfca7e6483fbfe49
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-02-17 23:22:07 +0100

    Formatting and more convenient API

--- src/reminders/core.clj
+++ src/reminders/core.clj
@@ -1,18 +1,38 @@
 (ns reminders.core
   (:use [clojure.string :only [split join]])
-  (:import [org.joda.time DateTime]))
+  (:import [org.joda.time DateTime]
+           [java.text SimpleDateFormat]))
 
-(defn add [desc when reminders]
+(def reminders (ref []))
+
+(defn add [desc when]
   (letfn [(reverse-date [date]
             (join \- (reverse (split date #"-"))))]
-    (conj reminders [(new DateTime (reverse-date when)) desc])))
+    (dosync
+     (alter reminders conj [(new DateTime (reverse-date when)) desc]))))
 
-(defn filter-near [days reminders]
+(defn filter-near [days]
   (letfn [(near? [date days]
             (if (. (new DateTime) isAfter (. date minusDays days))
               true
               false))]
-    (sort (filter #(near? (first %) days) reminders))))
+    (sort (filter #(near? (first %) days) @reminders))))
 
-;; TODO Use atoms to make this functions easier to use through a REPL
+(defn format-date-time [date]
+  (. (new SimpleDateFormat "dd/MM/yyyy") format (. date toDate)))
+
+(defn format-reminders [reminders]
+  (letfn [(format-reminder [reminder]
+            (str (format-date-time (first reminder)) " - " (first (rest reminder))))]
+    (join \newline (map format-reminder reminders))))
+
+(defn show-me
+  ([] (println (format-reminders @reminders)))
+  ([days] (println (format-reminders (filter-near days)))))
+
+;; ---- Usage
+;;(add "Read SICP" "22-08-2012")
+;;(show-me)
+;;(show-me 7)
+
 ;; TODO Implement persistence
